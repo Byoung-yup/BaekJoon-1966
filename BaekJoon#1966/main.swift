f7ea0265edb8a90f27eacee140d735c2ae3fd4cb
Dev_Ozzy <20170926@vision.hoseo.edu>
2023-02-07 02:41:19 +0900

    commit

--- BaekJoon#1966/main.swift
+++ BaekJoon#1966/main.swift
@@ -4,8 +4,53 @@
 //
 //  Created by 김병엽 on 2023/02/07.
 //
+// Reference: https://velog.io/@yc1303/백준-Python-Swift-1966번-프린터-큐
 
 import Foundation
 
-print("Hello, World!")
+func solution() {
+    
+    let testCase = Int(readLine()!)!
+    
+    for _ in 0..<testCase {
+        
+        let inputNM = readLine()!.split(separator: " ").map { Int(String($0))! }
+        let M = inputNM[1]
+        
+        var printCount = 0
+        
+        var tempPriorityList = readLine()!.split(separator: " ").map { Int(String($0))! }
+        var priorityList: [(Int, Int)] = []
+        
+        for (index, priority) in tempPriorityList.enumerated() {
+            
+            let tuple = (priority, index)
+            priorityList.append(tuple)
+            
+        }
+        
+        tempPriorityList.sort()
+        
+        while true {
+            
+            if priorityList[0].0 == tempPriorityList.max() {
+                printCount += 1
+                
+                if priorityList[0].1 == M {
+                    print(printCount)
+                    break
+                } else {
+                    tempPriorityList.removeLast()
+                    priorityList.removeFirst()
+                }
+            } else {
+                priorityList.append(priorityList.removeFirst())
+            }
+            
+        }
+        
+    }
+    
+}
 
+solution()
